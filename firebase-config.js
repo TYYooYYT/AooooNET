b0edc523a8d2dbe8113c415012073c50a09fe3aa
TYYooYYT <ahmedwaddah772@gmail.com>
2025-08-06 22:29:59 +0300

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,21 @@
-// إعداد Firebase
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB2RJfr0JDcPULeYzWkRxvFDNJPw-eAdz8",
-  authDomain: "ahmed-al-nageeb.firebaseapp.com",
-  projectId: "ahmed-al-nageeb",
-  storageBucket: "ahmed-al-nageeb.appspot.com",
-  messagingSenderId: "392333502263",
-  appId: "1:392333502263:web:63e16e17a5a4c2e4c25cb2",
-  measurementId: "G-ZWHC2TFL1W"
+  apiKey: "AIzaSyAv8OEQxjA7DwIuUWqwjhR3pjdmHw9BOuY",
+  authDomain: "al-nageeb-stm.firebaseapp.com",
+  projectId: "al-nageeb-stm",
+  storageBucket: "al-nageeb-stm.firebasestorage.app",
+  messagingSenderId: "1096369666448",
+  appId: "1:1096369666448:web:e0a7cc36ed33a8efc01b15",
+  measurementId: "G-3PT7LZE732"
 };
-firebase.initializeApp(firebaseConfig);
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
